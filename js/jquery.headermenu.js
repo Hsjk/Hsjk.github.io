429ee79cde197275c468e0632bd02d5ffbc6fd3e
Hsjk <shogo.matsuo@uec.ac.jp>
2017-07-14 17:37:22 +0900

    復活！

--- js/jquery.headermenu.js
+++ js/jquery.headermenu.js
@@ -17,4 +17,26 @@ function yomikomu() {
         });
     });
 }
+
+function yomikomu2() {
+    $(function () {
+        $.ajax({
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType('text/html;charset=UTF-8');
+            },
+            type: "GET",
+            url: "globalmenu.html",
+            timeout: 10000
+        })
+        .done(function (response, textStatus, jqXHR) {
+            console.log("Ç¢Ç¶Å[Ç¢")
+            $("#g_menu").html(response)
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("ÇµÇ¡ÇœÇ¢")
+        });
+    });
+}
+
 yomikomu();
+yomikomu2();
