fbf76c0a9699437ff783fdd9534f246cbb01da95
Hsjk <shogo.matsuo@uec.ac.jp>
2017-07-14 17:17:46 +0900

    jsちょっといじったよ

---
+++ js/jquery.headermenu.js
@@ -0,0 +1,20 @@
+function yomikomu() {
+    $(function () {
+        $.ajax({
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType('text/html;charset=UTF-8');
+            },
+            type: "GET",
+            url: "globalhead.html",
+            timeout: 10000
+        })
+        .done(function (response, textStatus, jqXHR) {
+            console.log("Ç¢Ç¶Å[Ç¢")
+            $("#g_header").html(response)
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("ÇµÇ¡ÇœÇ¢")
+        });
+    });
+}
+yomikomu();
